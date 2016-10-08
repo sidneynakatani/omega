5e113814fc755e1685f3cd3565fa85484c73dac4
sidney <sidney.nakatani@hotmail.com>
2016-10-08 01:40:30 -0300

    testes

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Login(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+    def post(self):
+        user = request.form['user']
+        password = request.form['pass']
+        auth = False        
+
+    
+        if(user == 'sid' and password == 'cat'):
+             auth = True 
+
+
+        return {'auth': auth}
+
+
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
