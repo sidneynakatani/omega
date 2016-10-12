07d1d5114d727c87f421e8772972e096c03098d2
sidney <sidney.nakatani@hotmail.com>
2016-10-12 03:18:00 -0300

    teste

--- db/connectionfactory.py
+++ db/connectionfactory.py
@@ -4,14 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import QueuePool
 import os
 
-#user     = os.environ['USER']
-#password = os.environ['PASS']
-#host     = os.environ['HOST']
-#port     = os.environ['PORT']
-#database = os.environ['DATABASE']
+user     = os.environ.get('USER')
+password = os.environ.get('PASS')
+host     = os.environ.get('HOST')
+port     = os.environ.get('PORT')
+database = os.environ.get('DATABASE')
 
-#url = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format{user, password, host, port, database}
-url = 'postgresql://indhzlkmaahykf:JQ58qiRbALZDmmR2jIMt1b0iRc@ec2-54-83-40-119.compute-1.amazonaws.com:5432/daf71h3n91rguu'
+url = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format{user, password, host, port, database}
 
 engine = create_engine(url, poolclass=QueuePool)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
