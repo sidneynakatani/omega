ce20455f6e76d77aa21f32266681bfb9b56737ce
sidney <sidney.nakatani@hotmail.com>
2016-10-24 02:47:30 -0200

    teste

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Login(Resource):
 
 
 api.add_resource(Login, '/login')
-api.add_resource(RegisterApi, '/register', resource_class_kwargs={'request': request})
+#api.add_resource(RegisterApi, '/register', resource_class_kwargs={'request': request})
 
 if __name__ == '__main__':
     app.run(debug=True)
