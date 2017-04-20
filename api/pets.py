3e3c43dcc4cbd9d5e5ed5ba55454f2294eab0ff4
sidney <sidney.nakatani@hotmail.com>
2017-04-20 01:06:45 -0300

    join address with pet

--- api/pets.py
+++ api/pets.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import request
 from model.pet import Pet
+from model.address import Address
 from db.connectionfactory import db_session
 from flask import jsonify
 
@@ -17,7 +18,7 @@ class PetsApi(Resource):
               
 	try:
 	   
-            result = db_session.query(Pet.id, Pet.pet_name, Pet.created_date, Pet.pet_kind, Pet.status).filter_by(credential_id = user_code).all()
+	    result = db_session.query(Pet.id, Pet.pet_name, Pet.created_date, Pet.pet_kind, Pet.status, Pet.image_id, Address.city).filter_by(credential_id = user_code).filter(Pet.id == Address.pet_id).all()
          
         except:	          
             print 'Erro ao executar a busca.'
